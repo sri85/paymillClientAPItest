d898ffab6790736dd895ceaee50ffabfa3089d90
sri85 <paisripathi@gmail.com>
2014-09-14 18:56:48 +0200

    Cleaned up the remove  API test code
    1. Added Documentation
    2. Added comments

--- paymillClient2Deletespec.js
+++ paymillClient2Deletespec.js
@@ -1,58 +1,57 @@
 var frisby = require('frisby');
 
-// These tests are for testing paymill create API
+// These tests are for testing paymill remove Client https://developers.paymill.com/en/reference/api-reference/index.html#remove-client
 var serverDetails = {
 "url": "https://api.paymill.com/v2.1/clients/",
 "firstEmail":"first@test.com",
 "secondEmail":"second@test.com",
 
-
 };
-
+//Global setup which will be used for all the requests
 frisby.globalSetup({ 
   request: {
     headers: { 'Authorization': 'Basic ZjlmYjkzZTYwOWQ0ZDhlNmRiMDRiZmQ0MDBiYjM2MTU6Og==' }
   }
 });
 
-frisby.create("")
+//Setting up test data
+frisby.create("Setting up the test data")
 .post(serverDetails.url,{
             email:serverDetails.firstEmail,
             
       })
       .afterJSON(function(response){
-//Step3: This is the step where we cleanup the data.
+//Step2: This is the step where we cleanup the data.
       frisby.create("")
+      
       .delete(serverDetails.url+response.data.id)
       .toss()
       })
       .afterJSON(function(response){
- //STEP2: This is the step where we get the details of the client where which we had created in the previous state.
-      frisby.create("Check whether get client details API returns a 200 when proper client details are passed ")
+   
+ //STEP3: This is the step where we get the details of the client where which we had created in the previous state.
+      frisby.create("Check whether the client has been deleted  properly by checking it returns a 404 error after deletion ")
       .get(serverDetails.url+response.data.id)
       .expectStatus(404)
       .expectHeaderContains('content-type','application/json')
       
       .toss()
-      })
-      .toss()
-      
-
-
-frisby.create("")
+      }).toss();
+//Create test data
+frisby.create("Create Test Data")
 .post(serverDetails.url,{
             email:serverDetails.secondEmail,
             
       })
       .afterJSON(function(response){
-//Step3: This is the step where we cleanup the data.
-      frisby.create("")
+//Step2: This is the step where we cleanup the data.
+      frisby.create("Delete the data")
       .delete(serverDetails.url+response.data.id)
       .toss()
       })
       .afterJSON(function(response){
  //STEP2: This is the step where we get the details of the client where which we had created in the previous state.
-      frisby.create("Check whether get client details API returns a 200 when proper client details are passed ")
+      frisby.create("Check the message returned by the API when passing a non existent id to the delete API")
       .delete(serverDetails.url+response.data.id)
       .expectStatus(404)
       .expectJSON({
@@ -61,35 +60,35 @@ frisby.create("")
     })
       
     .toss()
-    })
-    .toss()
+    }).toss();
+    
 
-frisby.create("")
+frisby.create("Create test data")
 .post(serverDetails.url,{
             email:serverDetails.firstEmail,
             
       })
       .afterJSON(function(response){
 //Step3: This is the step where we cleanup the data.
-      frisby.create("")
+      frisby.create("Chec whether the API returns 401 error when the delete API is called without authorization headers")
       .delete(serverDetails.url+response.data.id)
       .addHeader('Authorization', '')
       .expectStatus(401)
       .expectJSON({"error":"Access Denied","exception":"InvalidAuthentication"
       })
       .toss()
-      })
+      }).toss();
       
-    .toss()
+    
  
- frisby.create("")
+ frisby.create("Create test data for the test")
 .post(serverDetails.url,{
             email:serverDetails.secondEmail,
             
       })
       .afterJSON(function(response){
 //Step3: This is the step where we cleanup the data.
-      frisby.create("")
+      frisby.create("Check whether the API returns 412 error when delete API is called without passing the mandatory attribute(i.e client ID)")
       .delete(serverDetails.url)
       
       .expectStatus(412)
@@ -102,11 +101,11 @@ frisby.create("")
     }
 })
       .toss()
-      })
+      }).toss();
       
-    .toss();
     
     
+//Helper test to clean up all the remaining test data    
 frisby.create("")
         .get("https://api.paymill.com/v2.1/clients/")
         
